733b6edbb12e7ae401526f73804cffe21a07aa48
rodriguez-miguel-angel <rodriguez.miguel.angel@gmail.com>
2024-01-31 11:17:49 -0800

    second commit

---
+++ app/src/main/java/com/example/littlelemon/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.littlelemon.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object LittleLemonColor {
+    // step-04-01: Set up the Little Lemon colors
+    val green = Color(0xFF495E57)
+    val yellow = Color(0xFFF4CE14)
+    val pink = Color(0xFFFBDABB)
+    val cloud = Color(0xFFEDEFEE)
+    val charcoal = Color(0xFF333333)
+}
